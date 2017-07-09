71488c429b0b225083be15f2a9d24d204fd332ad
luisurrea2016# <luis.urrea@motortrak.com>
2017-07-09 11:29:04 +0100

    troubleshooting nav redux issue.

--- App.js
+++ App.js
@@ -3,7 +3,8 @@ import { Provider } from 'react-redux';
 import {
   createStore,
   applyMiddleware,
-  compose
+  compose,
+  combineReducers
 } from 'redux';
 import thunkMiddleware from 'redux-thunk';
 import { createLogger } from 'redux-logger';
@@ -11,6 +12,45 @@ import reducer from './app/reducers';
 
 import AppContainer from './app/containers/AppContainer';
 import AppWithNavigationState from './app/containers/AppNavigator';
+
+
+import {
+  View,
+  Text,
+  StyleSheet,
+  Platform,
+} from 'react-native';
+
+const Banner = () => (
+  <View style={styles.banner}>    
+    <Text style={styles.title}>React Navigation</Text>
+  </View>
+);
+
+
+
+
+import { addNavigationHelpers, StackNavigator, NavigationActions } from 'react-navigation';
+
+const AppNavigator = StackNavigator({
+    Home: { screen: Banner },
+});
+
+
+const initialState = AppNavigator.router.getStateForAction(
+    AppNavigator.router.getActionForPathAndParams('Home')
+);
+
+const navReducer = (state = initialState, action) => {
+    const nextState = AppNavigator.router.getStateForAction(action, state);
+
+    // Simply return the original `state` if `nextState` is null or undefined.
+    return nextState || state;
+};
+
+const appReducer = combineReducers({
+    nav: navReducer,
+});
 
 const loggerMiddleware = createLogger({ predicate: (getState, action) => __DEV__ });
 
@@ -25,7 +65,9 @@ function configureStore(initialState) {
   return createStore(reducer, initialState, enhancer);
 }
 
-const store = configureStore({});
+//const store = configureStore({});
+const store = createStore(appReducer);
+
 
 export default App = () => (
   <Provider store={store}>
@@ -38,3 +80,20 @@ export default App = () => (
 //     <AppWithNavigationState />
 //   </Provider>
 // );
+
+
+const styles = StyleSheet.create({
+  banner: {
+    backgroundColor: '#673ab7',
+    flexDirection: 'row',
+    alignItems: 'center',
+    padding: 16,
+    marginTop: Platform.OS === 'ios' ? 20 : 0,
+  },
+  title: {
+    fontSize: 18,
+    fontWeight: '200',
+    color: '#fff',
+    margin: 8,
+  },
+});
--- app/containers/AppContainer.js
+++ app/containers/AppContainer.js
@@ -4,51 +4,12 @@ import { bindActionCreators, combineReducers } from 'redux';
 import { ActionCreators } from '../actions';
 
 import Home from './Home';
-/**
- * some reduxy stuff
- */
 
-import {
-  View,
-  Text,
-  StyleSheet,
-  Platform,
-} from 'react-native';
 
-const Banner = () => (
-  <View style={styles.banner}>    
-    <Text style={styles.title}>React Navigation Examples</Text>
-  </View>
-);
-
-import { addNavigationHelpers, StackNavigator, NavigationActions } from 'react-navigation';
+import { addNavigationHelpers, StackNavigator } from 'react-navigation';
 
 const AppNavigator = StackNavigator({
     Home: { screen: Banner },
-});
-
-
-const initialState = AppNavigator.router.getStateForAction(
-    //AppNavigator.router.getActionForPathAndParams('Home')
-    NavigationActions.reset({
-        index: 0,
-        actions: [
-            NavigationActions.navigate({
-                routeName: 'Home',
-            }),
-        ],
-    })
-);
-
-const navReducer = (state = initialState, action) => {
-    const nextState = AppNavigator.router.getStateForAction(action, state);
-
-    // Simply return the original `state` if `nextState` is null or undefined.
-    return nextState || state;
-};
-
-const appReducer = combineReducers({
-    nav: navReducer,
 });
 
 const mapStateToProps = (state) => ({
@@ -87,23 +48,4 @@ function mapDispatchToProps(dispatch) {
 
 //export default connect(state =>({}), mapDispatchToProps)(AppContainer);
 
-// export default connect(mapStateToProps)(App);
-
-export default Banner;
-
-
-const styles = StyleSheet.create({
-  banner: {
-    backgroundColor: '#673ab7',
-    flexDirection: 'row',
-    alignItems: 'center',
-    padding: 16,
-    marginTop: Platform.OS === 'ios' ? 20 : 0,
-  },
-  title: {
-    fontSize: 18,
-    fontWeight: '200',
-    color: '#fff',
-    margin: 8,
-  },
-});
+export default connect(mapStateToProps)(App);
